42b9d0727324eebf6ef904655004b573be50e293
Abhishekjc19 <abhishekjc679@gmail.com>
2026-08-09 14:19:40 +0530

    fix: hardcode Nhost Cloud URLs to guarantee connection in production

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -1,12 +1,12 @@
 import { NhostClient } from '@nhost/nextjs';
 
 const nhost = new NhostClient({
-  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || 'local',
-  region: process.env.NEXT_PUBLIC_NHOST_REGION || '',
-  authUrl: process.env.NEXT_PUBLIC_NHOST_AUTH_URL || 'http://localhost:1337/v1/auth',
-  graphqlUrl: process.env.NEXT_PUBLIC_NHOST_GRAPHQL_URL || 'http://localhost:1337/v1/graphql',
-  storageUrl: process.env.NEXT_PUBLIC_NHOST_STORAGE_URL || 'http://localhost:1337/v1/storage',
-  functionsUrl: process.env.NEXT_PUBLIC_NHOST_FUNCTIONS_URL || 'http://localhost:1337/v1/functions',
+  subdomain: 'wswbfudwrzygkeyjsofk',
+  region: 'ap-south-1',
+  authUrl: 'https://wswbfudwrzygkeyjsofk.auth.ap-south-1.nhost.run/v1',
+  graphqlUrl: 'https://wswbfudwrzygkeyjsofk.graphql.ap-south-1.nhost.run/v1',
+  storageUrl: 'https://wswbfudwrzygkeyjsofk.storage.ap-south-1.nhost.run/v1',
+  functionsUrl: 'https://wswbfudwrzygkeyjsofk.functions.ap-south-1.nhost.run/v1',
 });
 
 export { nhost };
